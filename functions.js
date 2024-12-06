3f08d840fe58c05288b58d6d133e508f057f999e
Kristupas2004 <144653868+Kristupas2004@users.noreply.github.com>
2024-12-06 17:17:23 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -3,3 +3,22 @@ function toggleMode() {
     document.body.classList.toggle('dark-mode');
 }
 document.getElementById('mode').addEventListener('click', toggleMode);
+
+function toggleMode() {
+    document.body.classList.toggle('dark-mode');
+}
+document.getElementById('mode').addEventListener('click', toggleMode);
+
+function updateClock() {
+    const clockElement = document.getElementById('clock');
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    clockElement.textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+
+setInterval(updateClock, 1000);
+
+updateClock();
